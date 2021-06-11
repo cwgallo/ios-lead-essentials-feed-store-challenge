8ee4c945bd37cefa88bac558e96a85549b38be18
Chad Galloway <chad.galloway@peoplemedia.com>
2021-06-11 01:05:51 -0500

    core data entities and class definitions

---
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -0,0 +1,18 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Chad Galloway on 6/11/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+ @objc(ManagedFeedImage)
+ class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+ }
