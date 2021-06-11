c4c521f1dab7459ec479ecbc0515f05628a7574c
Chad Galloway <chad.galloway@peoplemedia.com>
2021-06-11 01:43:57 -0500

    Delete delivers error on deletion error

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -63,9 +63,13 @@ public final class CoreDataFeedStore: FeedStore {
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		let context = self.context
 		context.perform {
-			try! ManagedCache.find(in: context).map(context.delete)
-			try! context.save()
-			completion(nil)
+			do {
+				try ManagedCache.find(in: context).map(context.delete)
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
 		}
 	}
 }
